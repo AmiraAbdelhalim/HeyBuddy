4d000891c98a9bc2f3bd7af2f846852cb9a66453
amira <amira.a.a.hashim@gmail.com>
2020-08-04 04:52:27 +0200

    configure nd create helpers, change include all helpers to false, split collapsible elments

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module HeyBuddy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    
+    #helper available for corresponding views only
+    config.action_controller.include_all_helpers = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
